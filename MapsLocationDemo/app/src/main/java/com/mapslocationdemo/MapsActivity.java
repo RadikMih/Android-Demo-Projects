971b29320052bce20d26531d70ec070563b8ace5
RadikMih <radik.mihnev@gmail.com>
2019-06-09 21:21:40 +0300

    user address is shown as Toast

--- MapsLocationDemo/app/src/main/java/com/mapslocationdemo/MapsActivity.java
+++ MapsLocationDemo/app/src/main/java/com/mapslocationdemo/MapsActivity.java
@@ -3,6 +3,8 @@ package com.mapslocationdemo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,6 +13,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,6 +22,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -63,6 +70,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+
         mMap = googleMap;
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
@@ -72,7 +81,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.clear();
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listAddresses= geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if (listAddresses != null && listAddresses.size() > 0) {
+
+                        String address = "";
+
+                        if (listAddresses.get(0).getSubThoroughfare() != null) {
+                            address += listAddresses.get(0).getSubThoroughfare() + " ";
+                        }
+
+                        if (listAddresses.get(0).getThoroughfare() != null) {
+                            address += listAddresses.get(0).getThoroughfare() + " ";
+                        }
+
+                        if (listAddresses.get(0).getThoroughfare() != null) {
+                            address += listAddresses.get(0).getThoroughfare() + ", ";
+                        }
+
+                        if (listAddresses.get(0).getLocality() != null) {
+                            address += listAddresses.get(0).getLocality() + ", ";
+                        }
+
+                        if (listAddresses.get(0).getPostalCode() != null) {
+                            address += listAddresses.get(0).getPostalCode() + ", ";
+                        }
+
+                        if (listAddresses.get(0).getCountryName() != null) {
+                            address += listAddresses.get(0).getCountryName();
+                        }
+
+                        Toast.makeText(MapsActivity.this, address, Toast.LENGTH_LONG).show();
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
 
             @Override

9f5c6ae6bc854b25df9705e4908b3ab323fb779d
RadikMih <radik.mihnev@gmail.com>
2019-10-30 14:16:17 +0200

    new activity

---
+++ MultipleActivitiesDemo/app/src/main/java/com/multipleactivitiesdemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.multipleactivitiesdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        final List<String> friends = new ArrayList<>();
+        friends.add("Monica");
+        friends.add("Chandler");
+        friends.add("Joey");
+        friends.add("Rachel");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, friends);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("name", friends.get(position));
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
---
+++ MultipleActivitiesDemo/app/src/main/java/com/multipleactivitiesdemo/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.multipleactivitiesdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+
+        Toast.makeText(this, intent.getStringExtra("name"), Toast.LENGTH_LONG).show();
+    }
+
+    public void toFirstActivity(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+}

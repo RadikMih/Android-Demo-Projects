cd60aec4d0e471bea2b2e24923196a9f74bcbce3
RadikMih <radik.mihnev@gmail.com>
2019-10-31 10:53:17 +0200

    adding shared preferences to the app

--- MemorablePlaces/app/src/main/java/com/memorableplaces/MainActivity.java
+++ MemorablePlaces/app/src/main/java/com/memorableplaces/MainActivity.java
@@ -1,6 +1,8 @@
 package com.memorableplaces;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,12 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    static List<String> places = new ArrayList<>();
-    static List<LatLng> locations = new ArrayList<>();
+    static ArrayList<String> places = new ArrayList<>();
+    static ArrayList<LatLng> locations = new ArrayList<>();
     static ArrayAdapter arrayAdapter;
 
     @Override
@@ -26,9 +29,46 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.listView);
 
-        places.add("Add a new place...");
-        locations.add(new LatLng(0, 0));
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.memorableplaces",
+                Context.MODE_PRIVATE);
 
+        ArrayList<String> latitudes = new ArrayList<>();
+        ArrayList<String> longitudes = new ArrayList<>();
+
+        places.clear();
+        locations.clear();
+        latitudes.clear();
+        longitudes.clear();
+
+        try {
+            places = (ArrayList<String>) ObjectSerializer
+                    .deserialize(sharedPreferences.getString("places",
+                    ObjectSerializer.serialize(new ArrayList<String>())));
+
+            latitudes = (ArrayList<String>) ObjectSerializer
+                    .deserialize(sharedPreferences.getString("latitudes",
+                    ObjectSerializer.serialize(new ArrayList<String>())));
+
+            longitudes = (ArrayList<String>) ObjectSerializer
+                    .deserialize(sharedPreferences.getString("longitudes",
+                    ObjectSerializer.serialize(new ArrayList<String>())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (places.size() > 0 && latitudes.size() > 0 && longitudes.size() > 0){
+            if (places.size() == latitudes.size() && latitudes.size() == longitudes.size()) {
+                for (int i = 0; i < places.size(); i++) {
+                    locations.
+                            add(new LatLng(Double.parseDouble(latitudes.get(i))
+                                    ,Double.parseDouble(longitudes.get(i))));
+                }
+            }
+        } else {
+            places.add("Add a new place...");
+            locations.add(new LatLng(0, 0));
+        }
+        
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
         listView.setAdapter(arrayAdapter);
 
--- MemorablePlaces/app/src/main/java/com/memorableplaces/MapsActivity.java
+++ MemorablePlaces/app/src/main/java/com/memorableplaces/MapsActivity.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -26,6 +27,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -124,12 +126,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             placeLocation.setLatitude(MainActivity.locations.get(intent.getIntExtra("placeNumber", 0)).latitude);
             placeLocation.setLongitude(MainActivity.locations.get(intent.getIntExtra("placeNumber", 0)).longitude);
 
-
             centerMapOnLocation(placeLocation, MainActivity.places.get(intent.getIntExtra("placeNumber", 0)));
-
-//            mMap.addMarker(new MarkerOptions()
-//                    .position(MainActivity.locations.get(intent.getIntExtra("placeNumber", 0)))
-//                    .title(MainActivity.places.get(intent.getIntExtra("placeNumber", 0))));
         }
     }
 
@@ -162,6 +159,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         MainActivity.places.add(address);
         MainActivity.locations.add(latLng);
         MainActivity.arrayAdapter.notifyDataSetChanged();
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.memorableplaces",
+                Context.MODE_PRIVATE);
+        try {
+            ArrayList<String> latitudes = new ArrayList<>();
+            ArrayList<String> longitudes = new ArrayList<>();
+
+            for (LatLng coordinates : MainActivity.locations) {
+                latitudes.add(Double.toString(coordinates.latitude));
+                longitudes.add(Double.toString(coordinates.longitude));
+            }
+
+            sharedPreferences.edit().putString("places", ObjectSerializer.serialize(MainActivity.places)).apply();
+            sharedPreferences.edit().putString("latitudes", ObjectSerializer.serialize(latitudes)).apply();
+            sharedPreferences.edit().putString("longitudes", ObjectSerializer.serialize(longitudes)).apply();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         Toast.makeText(this, "Location saved", Toast.LENGTH_LONG).show();
     }
 }

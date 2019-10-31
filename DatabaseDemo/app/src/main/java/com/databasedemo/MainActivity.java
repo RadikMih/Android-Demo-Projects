168cd7b3eae4ce1b5a6cdf6ade582c070d3ed19f
RadikMih <radik.mihnev@gmail.com>
2019-10-31 17:24:11 +0200

    DB demo app

--- DatabaseDemo/app/src/main/java/com/databasedemo/MainActivity.java
+++ DatabaseDemo/app/src/main/java/com/databasedemo/MainActivity.java
@@ -16,9 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             SQLiteDatabase database = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
-            database.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
-            database.execSQL("INSERT INTO users (name, age) VALUES ('Test1', 30)");
-            database.execSQL("INSERT INTO users (name, age) VALUES ('Test2', 32)");
+         //   database.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+         //   database.execSQL("INSERT INTO users (name, age) VALUES ('Test3', 33)");
+         //   database.execSQL("INSERT INTO users (name, age) VALUES ('Test4', 34)");
+
+         // database.execSQL("DELETE FROM users WHERE name = 'Test4'");
+         // database.execSQL("update users set age = 34 where name = 'Test3'");
+
+            // WHERE age > 31 AND name LIKE '%4'
 
             Cursor c = database.rawQuery("SELECT * FROM users", null);
             int nameIndex = c.getColumnIndex("name");
@@ -26,15 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
             c.moveToFirst();
             while (c != null) {
-                Log.i("name", c.getString(nameIndex));
-                Log.i("age", Integer.toString(c.getInt(ageIndex)));
+                Log.i("User Result - name", c.getString(nameIndex));
+                Log.i("User Result - age", Integer.toString(c.getInt(ageIndex)));
                 c.moveToNext();
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 }

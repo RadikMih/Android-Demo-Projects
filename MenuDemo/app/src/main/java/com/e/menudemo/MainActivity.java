4450a9683a36fc675720111d18280f8757ac1029
RadikMih <radik.mihnev@gmail.com>
2019-10-31 11:07:18 +0200

    menu demo

---
+++ MenuDemo/app/src/main/java/com/e/menudemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.e.menudemo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Log.i("Menu item selected", "Settings");
+                return true;
+            case R.id.help:
+                Log.i("Menu item selected", "Help");
+                return true;
+            default:
+                   return false;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

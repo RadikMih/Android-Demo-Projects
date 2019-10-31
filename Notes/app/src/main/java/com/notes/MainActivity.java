943ac06e633c3582bfffab12721300b72d0b3fe9
RadikMih <radik.mihnev@gmail.com>
2019-10-31 14:44:27 +0200

    adding notes to the second activity

---
+++ Notes/app/src/main/java/com/notes/MainActivity.java
@@ -0,0 +1,44 @@
+package com.notes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        notes.add("Example note");
+
+        arrayAdapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                notes);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+                intent.putExtra("noteId", position);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ Notes/app/src/main/java/com/notes/NoteEditorActivity.java
@@ -0,0 +1,45 @@
+package com.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+        EditText editText = findViewById(R.id.editText);
+        Intent intent = getIntent();
+        final int noteId = intent.getIntExtra("noteId", -1);
+
+        if (noteId != -1) {
+            editText.setText(MainActivity.notes.get(noteId));
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence sequence, int start, int before, int count) {
+                MainActivity.notes.set(noteId, String.valueOf(sequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+}

cfff626183b606943fdc31a8f63a76c1173d7efb
RadikMih <radik.mihnev@gmail.com>
2019-11-04 17:32:59 +0200

    adding parser

---
+++ InstagramApp/app/src/main/java/com/instagramapp/StarterApplication.java
@@ -0,0 +1,52 @@
+package com.instagramapp;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+
+                .build()
+        );
+
+        ParseObject object = new ParseObject("ExampleObject");
+        object.put("myNumber", "123");
+        object.put("myString", "rob");
+
+        object.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException ex) {
+                if (ex == null) {
+                    Log.i("===== Parse Result", "Successful!");
+                } else {
+                    ex.printStackTrace();
+                    Log.i("===== Parse Result", "Failed " + ex.toString());
+                }
+            }
+        });
+
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}

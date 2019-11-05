4d5753e0ccc7f6fca13805cdee1a81cda21b9b16
RadikMih <radik.mihnev@gmail.com>
2019-11-05 10:30:12 +0200

    initial commit

---
+++ ParseTechniques/app/src/main/java/com/parsetechniques/MainActivity.java
@@ -0,0 +1,18 @@
+package com.parsetechniques;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.parse.ParseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+}

c161c50a54eae6156c70ad5b0036fe16ca36110c
RadikMih <radik.mihnev@gmail.com>
2019-11-06 16:48:34 +0200

    adding some properties

---
+++ Uber/app/src/main/java/com/uber/StarterApplication.java
@@ -0,0 +1,33 @@
+package com.uber;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("")
+                .clientKey("")
+                .server("")
+                .build()
+        );
+
+        //       ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}

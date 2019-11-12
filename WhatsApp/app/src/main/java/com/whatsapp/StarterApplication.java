7703780df96ead9db2d694c8925651d67b1beb99
RadikMih <radik.mihnev@gmail.com>
2019-11-12 10:33:51 +0200

    initial commit

---
+++ WhatsApp/app/src/main/java/com/whatsapp/StarterApplication.java
@@ -0,0 +1,34 @@
+package com.whatsapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("")
+                .clientKey("")
+                .server("")
+                .build()
+        );
+
+        //ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}

7a879778d3fe0a574b7dc3e5f0bfefb35ce2618b
RadikMih <radik.mihnev@gmail.com>
2019-11-22 11:50:03 +0200

    working recycler view on the main activity

--- Kotlin Demo Projects/BestQuotes/app/src/main/java/com/bestquotes/MainActivity.kt
+++ Kotlin Demo Projects/BestQuotes/app/src/main/java/com/bestquotes/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.bestquotes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var db: SQLiteDatabase? = null
+    var cursor: Cursor? = null
+    var categoriesAdapter: CategoriesAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bestQuotesDatabaseHelper = BestQuotesSQLiteOpenHelper(this)
+        db = bestQuotesDatabaseHelper.readableDatabase
+        cursor = db!!.query("quote_categories", arrayOf("_id", "image_resource_id", "name"),
+            null, null, null, null, null)
+
+        var listOfCategories = mutableListOf<Category>()
+        while (cursor!!.moveToNext()) {
+            val categoryId = cursor!!.getInt(0)
+            val categoryResourceId = cursor!!.getInt(1)
+            val categoryName = cursor!!.getString(2)
+
+            val category: Category = Category(categoryId, categoryResourceId, categoryName)
+            listOfCategories.add(category)
+        }
+
+        categoriesAdapter = CategoriesAdapter(this, listOfCategories)
+
+       // val categoriesLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+         val categoriesLayoutManager = GridLayoutManager(this, 2)
+
+        categoriesRecyclerView.adapter = categoriesAdapter
+        categoriesRecyclerView.layoutManager = categoriesLayoutManager
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        db!!.close()
+        cursor!!.close()
     }
 }
---
+++ Kotlin Demo Projects/BestQuotes/app/src/main/java/com/bestquotes/QuotesDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.bestquotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuotesDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes_details)
+    }
+}

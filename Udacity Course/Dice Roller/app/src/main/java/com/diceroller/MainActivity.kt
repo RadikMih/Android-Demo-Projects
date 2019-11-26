64c2c2c9c8f2c901d7a912f93888d97f971e7508
RadikMih <radik.mihnev@gmail.com>
2019-11-26 14:44:37 +0200

    adding images

--- Udacity Course/Dice Roller/app/src/main/java/com/diceroller/MainActivity.kt
+++ Udacity Course/Dice Roller/app/src/main/java/com/diceroller/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = roll_button as Button
+        rollButton.setOnClickListener{
+            // Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText = result_text as TextView
+
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }

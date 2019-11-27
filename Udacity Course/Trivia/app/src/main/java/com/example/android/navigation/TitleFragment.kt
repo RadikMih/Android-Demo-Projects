2aee5ed7d81a055ee0b0ba139e24dc2b96336bcf
RadikMih <radik.mihnev@gmail.com>
2019-11-27 17:12:13 +0200

    adding safe arguments

--- Udacity Course/Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Udacity Course/Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -19,8 +19,11 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false
         )
-        binding.playButton.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        binding.playButton.setOnClickListener{ view: View ->
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+
+        }
+
         setHasOptionsMenu(true)
         return binding.root
     }

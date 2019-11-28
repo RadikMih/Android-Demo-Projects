1d04c22dbf9228a11c04889ede0e5ae1ba762b6f
RadikMih <radik.mihnev@gmail.com>
2019-11-28 10:07:46 +0200

    adding animation to navigation

--- Udacity Course/Trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Udacity Course/Trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : AppCompatActivity() {
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
             }
         }
-
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
 

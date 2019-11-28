071c00216c31389e7d26ec9d341229efb4fd0722
RadikMih <radik.mihnev@gmail.com>
2019-11-28 17:21:45 +0200

    initial commit

---
+++ Udacity Course/Guess it/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -0,0 +1,44 @@
+package com.example.android.guesstheword.screens.score
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.ScoreFragmentBinding
+
+/**
+ * Fragment where the final score is shown, after the game is over
+ */
+class ScoreFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate view and obtain an instance of the binding class.
+        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.score_fragment,
+                container,
+                false
+        )
+
+        // Get args using by navArgs property delegate
+        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+        binding.scoreText.text = scoreFragmentArgs.score.toString()
+        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+
+        return binding.root
+    }
+
+    private fun onPlayAgain() {
+        findNavController().navigate(ScoreFragmentDirections.actionRestart())
+    }
+}

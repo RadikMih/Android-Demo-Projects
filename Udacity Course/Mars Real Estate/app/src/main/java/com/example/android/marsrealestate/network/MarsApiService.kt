cadf500c464f3a4b5bf232c1998a68863a24ed37
RadikMih <radik.mihnev@gmail.com>
2019-12-03 14:12:23 +0200

    adding retrieving number of properties

--- Udacity Course/Mars Real Estate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ Udacity Course/Mars Real Estate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -1,28 +1,34 @@
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
-// Using Retrofit Builder with ScalarsConverterFactory and BASE_URL
-private val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .baseUrl(BASE_URL)
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
         .build()
 
-// Implementing the MarsApiService interface with @GET getProperties returning a String
-interface MarsApiService {
-    @GET("realestate")
-    fun getProperties():
-            Call<String>
-}
+// Using Retrofit Builder with ScalarsConverterFactory and BASE_URL
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
 
 // Creating the MarsApi object using Retrofit to implement the MarsApiService
 object MarsApi {
     val retrofitService: MarsApiService by lazy {
         retrofit.create(MarsApiService::class.java)
     }
+}
+
+// Implementing the MarsApiService interface with @GET getProperties returning a String
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties():
+            Call<List<MarsProperty>>
 }

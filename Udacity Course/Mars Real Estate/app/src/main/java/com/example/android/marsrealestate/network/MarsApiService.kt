b3291ed3b9eaf65aab5c6287f8c999df50e12ddd
RadikMih <radik.mihnev@gmail.com>
2019-12-03 13:54:12 +0200

    adding MarsApiService

--- Udacity Course/Mars Real Estate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ Udacity Course/Mars Real Estate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -1,20 +1,28 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+// Using Retrofit Builder with ScalarsConverterFactory and BASE_URL
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+// Implementing the MarsApiService interface with @GET getProperties returning a String
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties():
+            Call<String>
+}
+
+// Creating the MarsApi object using Retrofit to implement the MarsApiService
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}

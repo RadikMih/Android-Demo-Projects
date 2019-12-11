edb98aebceb6fb6f527a277899c93421d665e329
RadikMih <radik.mihnev@gmail.com>
2019-12-11 19:33:04 +0200

    adding bottom nav bar

--- Udacity Course/Testing Views/app/src/main/java/com/testingviews/MainActivity.kt
+++ Udacity Course/Testing Views/app/src/main/java/com/testingviews/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.testingviews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val bottomNavigation = bottom_navigation as BottomNavigationView
+        bottomNavigation.setOnNavigationItemReselectedListener{ item ->
+            when(item.itemId) {
+                R.id.nav_home -> {}
+                R.id.nav_favorites -> {}
+                R.id.nav_search -> {}
+            }
+            return@setOnNavigationItemReselectedListener
+        }
+
     }
+
+
 }

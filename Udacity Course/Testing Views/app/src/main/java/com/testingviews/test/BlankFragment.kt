cb822cddb2e095a1fc668854ba67162b157291d8
RadikMih <radik.mihnev@gmail.com>
2020-02-14 12:17:42 +0200

    no change

--- Udacity Course/Testing Views/app/src/main/java/com/testingviews/test/BlankFragment.kt
+++ Udacity Course/Testing Views/app/src/main/java/com/testingviews/test/BlankFragment.kt
@@ -1,12 +1,10 @@
 package com.testingviews.test
 
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.testingviews.R
 
 
@@ -25,3 +23,4 @@ class BlankFragment : Fragment() {
             BlankFragment()
     }
 }
+
